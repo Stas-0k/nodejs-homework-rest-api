968c158948ba8839b9a043361ad5cdad67f99dda
Stas-0k <brickbybrick15@gmail.com>
2023-04-13 16:50:17 +0300

    commit
    commit

--- models/contacts.js
+++ models/contacts.js
@@ -50,7 +50,7 @@ const updateById = async (id, data) => {
     }
     contacts[index] = {...contacts[index], ...data};
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return contacts[index];
+  return contacts[index];
 }
 
 module.exports = { listContacts, getContactById, removeContact, addContact,updateById };
