b01100a6b1c11c3ad6ae5147aae7205aabb4ee27
Stas-0k <brickbybrick15@gmail.com>
2023-05-03 15:21:58 +0300

    commit
    commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
 const contactSchema = new Schema({
   name: {
@@ -25,8 +26,31 @@ const contactSchema = new Schema({
 
 const Contact = model("contact", contactSchema)
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+const updateSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
+
+const updateFavorite = Joi.object({
+  favorite: Joi.boolean(),
+})
+
+const schemas = {
+  addSchema,
+  updateSchema,
+  updateFavorite
+}
+
 
 
 module.exports = {
   Contact,
+  schemas,
 };
